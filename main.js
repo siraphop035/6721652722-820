6ee86530eedee7735f896e76ab9cb14f5792b143
siraphop035 <siraphop.sric@ku.th>
2025-12-29 16:50:46 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,61 @@
+alert ('Run')
+/*
+let n1 = 10;
+let n2 = 20;
+
+if (n1>n2) {
+    console.log('Number 1 > Number 2');
+} else if (n1<n2) {
+    console.log('Number 1 < Number 2');
+} else {
+    console.log('Number 1 = Number 2');
+}
+
+*/
+
+/*
+let score = prompt('Score');
+console.log(score)
+
+if (score >= 80){
+        console.log('Grade A');
+} else if (score >= 70 ) {
+        console.log('Grade B');
+} else if (score >= 60 ) {
+        console.log('Grade C');
+} else if (score >= 50 ) {
+        console.log('Grade D');
+} else {
+    console.log('Grade = F')
+}
+
+*/
+
+/*
+let number1 = 1;
+let number2 = 10;
+
+let condition = (number1 >= 3 || number2 >= 5);
+console.log(condition)
+*/
+
+
+let a = 1;
+
+/*
+for ( a; a <= 10; a++){
+    console.log(a)
+}
+
+
+while (a<=10){
+    console.log(a);
+    a++
+}
+
+
+do {
+    console.log(a);
+    a++
+} while (a<=10)
+*/
