ea91831274b00a8636361b1bbc0959f0d08cf079
siraphop035 <siraphop.sric@ku.th>
2026-03-14 15:37:20 +0700

    updata front

--- Frontend/index.js
+++ Frontend/index.js
@@ -64,9 +64,13 @@ const submitData = async () => {
     } catch (error) {
         console.log('error message', error.message);
         console.log('error', error.errors);
-      //  if (error.response) {
-      //      console.log('Error response:', error.response.data.message);
-      //  }
+        if (error.response) {
+            console.log('error response', error.response);
+            error.message = error.response.data.message;
+            error.errors = error.response.data.errors;
+
+        }
+
         let htmlData = '<div>'
         htmlData += `<div>${error.message}</div>`;
         htmlData += '<ul>';
@@ -81,7 +85,3 @@ const submitData = async () => {
         messageDOM.className = 'message danger';
     }
 }
-\
-
-
-
--- Frontend/user.js
+++ Frontend/user.js
@@ -1,32 +1,37 @@
-const Base_URL = 'http://localhost:8000';
+const BASE_URL = "http://localhost:8000";
 window.onload = async () => {
+    await loadData();
+}
 
-    const response = await axios.get(`${Base_URL}/users`);
-    console.log('response', response);
-    const userDOM = document.getElementById('user');
+const loadData = async () => {
+    const response = await axios.get(`${BASE_URL}/users`);
+    console.log(response.data);
+    const userDOM = document.getElementById("user");
     let htmlData = '<div>';
-    for (let i =0 ; i < response.data.length; i++) {
+    for (let i = 0; i < response.data.length; i++) {
         let user = response.data[i];
-        htmlData += `<div>
-        ${user.firstName} ${user.lastName} 
-        <button class='edit' data-id='${user.id}'>Edit</button>
+        htmlData += ` <div>
+        ${user.firstname} ${user.lastname}
+        <button>Edit</button>
         <button class='delete' data-id='${user.id}'>Delete</button>
         </div>`
     }
     htmlData += '</div>';
     userDOM.innerHTML = htmlData;
 
-    const deleteDoms = document.getElementsByClassName('delete');
-    for (let i = 0; i < deleteDoms.length; i++) {
-        deleteDoms[i].addEventListener('click', async (e) => {
+    const deleteDOMs = document.getElementsByClassName("delete");
+    for (let i = 0; i < deleteDOMs.length; i++) {
+        deleteDOMs[i].addEventListener("click", async (event) => {
+            // ดึง id ของ user ที่ต้องการลบจาก data-id attribute
             const id = event.target.dataset.id;
-            try {
-                await axios.delete(`${Base_URL}/users/${id}`);
-                loadData();
-            }catch (error) {
-                console.log('deleting error', error);
-            }
+            try{
+                await axios.delete(`${BASE_URL}/users/${id}`);
+                loadData(); // โหลดข้อมูลใหม่หลังจากลบสำเร็จ
+            }catch(error){
+                console.error("Error deleting user:", error);
+            }    
         });
     }
-
 }
+
+
